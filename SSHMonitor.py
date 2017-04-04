4c877e22ac3dff87bd138fdbe959788bf7fa5ab0
Anthony Guevara <amboxer21@gmail.com>
2017-04-03 21:54:47 -0400

    Fixed typo.

--- SSHMonitor.py
+++ SSHMonitor.py
@@ -106,7 +106,7 @@ def tail_file(logfile):
         if b:
             sys.stdout.write("banned - #{b.group(1)}")
             blocked_ip("banned",b.group(1))
-            send_mail(sender,to,password,port,'SSH IP Blocked',"#{b.group()} was banned for too many failed attempts")
+            send_mail(sender,to,password,port,'SSH IP Blocked',"#{b.group(1)} was banned for too many failed attempts")
             time.sleep(1)
 
 if len(sys.argv) > 4:
