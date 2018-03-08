bca4bcf2f254f15fc244f198e1237d4383bb7c04
amboxer21 <amboxer21@gmail.com>
2018-03-08 10:13:43 -0500

    Moved all build files into one dir and modified the build script.

--- build/build.sh
+++ build/build.sh
@@ -9,4 +9,5 @@ sudo pip install tailf
 sudo cp SSHMonitor.py /usr/bin/
 sudo chmod a+x /usr/bin/SSHMonitor.py
 sudo cp home/user/.ssh/is_sshm_running.sh /home/$USER/.ssh/
+sudo sed -i "s/user/$USER/g" root_crontab.txt;
 cat root_crontab.txt >> /var/spool/cron/crontabs/root
