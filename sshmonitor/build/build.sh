3442bbd2dd80712bad5b63b7461be62356e35809
amboxer21 <amboxer21@gmail.com>
2018-03-09 17:49:36 -0500

    Added echo statements to setup.py.

--- sshmonitor/build/build.sh
+++ sshmonitor/build/build.sh
@@ -2,28 +2,42 @@
 
 opt=$1;
 
-declare -a pip=(pytailf interpy)
 declare -a deb=(python python-dev python-setuptools python-pip build-essential)
 declare -a rpm=(python python-devel python-setuptools python-pip)
 
 function install() {
   manage_pkgs
   if [[ ! -d /home/root/.ssh ]]; then
+    echo -e "Dir /home/root/.ssh does not exist. Creating it now.\n";
     sudo mkdir -p /home/root/.ssh/;
   fi
+  echo -e "\nCopying sshmonitor/sshmonitor.py to /usr/bin/\n";
   sudo cp sshmonitor/sshmonitor.py /usr/bin/
+  echo -e "\nChanging permissions on /usr/bin/sshmonitor.py to a+x.\n";
   sudo chmod a+x /usr/bin/sshmonitor.py
+  echo -e "\nCopying sshmonitor/build/home/user/.ssh/is_sshm_running.sh to /home/root/.ssh/\n";
   sudo cp sshmonitor/build/home/user/.ssh/is_sshm_running.sh /home/root/.ssh/
+  echo -e "\nEditing sshmonitor/build/root_crontab.txt via sed.\n";
   sudo sed -i "s/user/$USER/g" sshmonitor/build/root_crontab.txt
   if [[ ! `egrep -io "\*.*is_sshm_running.sh$" /var/spool/cron/crontabs/root` ]]; then
+    echo -e "\nAdding to root crontab.\n";
     sudo cat sshmonitor/build/root_crontab.txt >> /var/spool/cron/crontabs/root
   fi
 };
 
 function remove() {
+  echo -e "\nRemoving /usr/bin/sshmonitor.py\n";
   sudo rm /usr/bin/sshmonitor.py 2> /dev/null
+  echo -e "\nRemoving /home/root/.ssh/is_sshm_running.sh\n";
   sudo rm /home/root/.ssh/is_sshm_running.sh 2> /dev/null
+  echo -e "\nEditing crontab /var/spool/cron/crontabs/root via sed.\n";
   sudo sed -i 's/^\*.*.sh$//g' /var/spool/cron/crontabs/root 2> /dev/null
+  for p in pytailf interpy; do
+    if [[ `sudo pip --no-cache-dir show $p | awk "/Name: $p/{print $2}"` ]]; then
+      echo -e "\nUninstalling $p pip package.\n";
+      sudo pip uninstall -y $p 2> /dev/null;
+    fi
+  done
 };
 
 function usage() {
@@ -34,32 +48,17 @@ function usage() {
 function manage_pkgs() {
   if [[ $pkgm == 'yum' ]]; then
     for y in "${rpm[@]}"; do
+      echo -e "\n${opt}-ing $opt via $pkgm.\n";
       sudo $pkgm -y $opt $y;
     done
   elif [[ $pkgm == 'apt-get' ]]; then
     for d in "${deb[@]}"; do
-      sudo apt-get -y $opt $d;
+      echo -e "\n${opt}-ing $opt via $pkgm.\n";
+      sudo $pkgm -y $opt $d;
     done
   else
     echo -e "Your package manager is not supported.";
   fi
-};
-
-function manage_pip() {
-  for p in "${pip[@]}"; do
-    case $1 in
-      'install')
-        if [[ ! `sudo pip --no-cache-dir show $p | awk "/Name: $p/{print $2}"` ]]; then
-          sudo pip $opt $p;
-        fi
-      ;;
-      'remove')
-        if [[ `sudo pip --no-cache-dir show $p | awk "/Name: $p/{print $2}"` ]]; then
-          sudo pip uninstall -y $p;
-        fi        
-      ;;
-    esac
-  done
 };
 
 if [[ ! $EUID == 0 ]]; then
@@ -70,6 +69,7 @@ fi
 for i in apt-get yum; do
   if [[ ! `$i --version 2> /dev/null` == '' ]]; then
     pkgm=$i;
+    echo -e "\nUsing the $pkgm package manager.\n";
   fi
 done
 
@@ -84,10 +84,8 @@ if [[ $opt == '' ]]; then
   usage;
 elif [[ $opt == 'install' ]]; then
   install;
-  #manage_pip 'install';
 elif [[ $opt == 'remove' ]]; then
   remove;
-  manage_pip 'remove';
 else 
   echo -e "\nERROR: $opt is not a valid arg!\n";
   usage;
