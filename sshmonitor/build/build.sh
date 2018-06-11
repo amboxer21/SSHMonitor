5292bfd1c4534399145f12d631414e416526b9ab
amboxer21 <amboxer21@gmail.com>
2018-06-11 11:18:51 -0400

    Added a function to check for cron file presence. Might need modding.

--- sshmonitor/build/build.sh
+++ sshmonitor/build/build.sh
@@ -5,6 +5,22 @@ opt=$1;
 declare -a deb=(python python-dev python-setuptools python-pip build-essential)
 declare -a rpm=(python python-devel python-setuptools python-pip)
 
+if [[ ! `echo $EUID` == 0 ]]; then echo -e "Must be root to run this build script."; exit; fi
+
+function cron_path() {
+  path='/var/spool/cron'
+  if [[ ! `which crontab 2> /dev/null` ]]; then echo -e "Cron is not installed. Exiting now."; exit; fi
+  if [[ -e `$path/crontabs 2> /dev/null` ]]; then 
+    cronpath="$path/crontabs";
+  elif [[ ! -e `$path/crontabs 2> /dev/null` ]]; then
+    cronpath='/var/spool/cron'
+  else
+    echo -e "Could not find cron path. Please manually add contents of file\n  sshmonitor/build/root_crontab.txt " 
+    echo -e "to your crontab. Exiting now.";
+    exit;
+  fi
+};
+
 function install() {
   manage_pkgs
   if [[ ! -d /home/root/.ssh ]]; then
@@ -17,19 +33,24 @@ function install() {
   sudo chmod a+x /usr/bin/sshmonitor.py
   echo -e "\nCopying sshmonitor/build/home/user/.ssh/is_sshm_running.sh to /home/root/.ssh/\n";
   sudo cp sshmonitor/build/home/user/.ssh/is_sshm_running.sh /home/root/.ssh/
-  if [[ ! `egrep -io "\*.*is_sshm_running.sh$" /var/spool/cron/crontabs/root` ]]; then
+  cron_path
+  if [[ ! `egrep -io "\*.*is_sshm_running.sh$" $cronpath/root` ]]; then
     echo -e "\nAdding to root crontab.\n";
-    sudo cat sshmonitor/build/root_crontab.txt >> /var/spool/cron/crontabs/root
+    sudo cat sshmonitor/build/root_crontab.txt >> $cronpath/root
+  fi
+  if [[ ! `egrep -io "\*.*is_sshm_running.sh$" $cronpath/root` ]]; then
+    echo -e "\n\nCould not install cron. Please manually install and restart the program!\n";
   fi
 };
 
 function remove() {
+  cron_path
   echo -e "\nRemoving /usr/bin/sshmonitor.py\n";
   sudo rm /usr/bin/sshmonitor.py 2> /dev/null
   echo -e "\nRemoving /home/root/.ssh/is_sshm_running.sh\n";
   sudo rm /home/root/.ssh/is_sshm_running.sh 2> /dev/null
-  echo -e "\nEditing crontab /var/spool/cron/crontabs/root via sed.\n";
-  sudo sed -i 's/^\*.*.sh$//g' /var/spool/cron/crontabs/root 2> /dev/null
+  echo -e "\nEditing crontab $cronpath/root via sed.\n";
+  sudo sed -i 's/^\*.*.sh$//g' $cronpath/root 2> /dev/null
   for p in pytailf interpy; do
     if [[ `sudo pip --no-cache-dir show $p | awk "/Name: $p/{print $2}"` ]]; then
       echo -e "\nUninstalling $p pip package.\n";
