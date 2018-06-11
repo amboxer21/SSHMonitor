853ce6ce066a8d9863352eed6671b610acf44dc2
amboxer21 <amboxer21@gmail.com>
2018-06-11 11:25:44 -0400

    Added app password since I am now using 2fa with this email addr\.

--- sshmonitor/build/home/user/.ssh/is_sshm_running.sh
+++ sshmonitor/build/home/user/.ssh/is_sshm_running.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ `ps aux | egrep --color -i "root.*[0-9]*:[0-9]* sudo /usr/bin/python /usr/bin/sshmonitor.py" | wc -l` < 1 ]]; then 
-  sudo /usr/bin/python /usr/bin/sshmonitor.py -e "example@gmail.com" -p "password" & 
+  sudo /usr/bin/python /usr/bin/sshmonitor.py -e "sshmonitorapp@gmail.com" -p "hkeyscwhgxjzafvj" & 
 fi
 
 if [[ `ps aux | egrep --color -i "root.*[0-9]*:[0-9]* sudo /usr/bin/python /usr/bin/sshmonitor.py" | wc -l` > 1 ]]; then 
