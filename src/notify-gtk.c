09e5c3af149cfddaf255754bd7cccd0891e81588
amboxer21 <amboxer21@gmail.com>
2019-08-23 15:28:02 -0400

    Added scrolled window to ui.

--- src/notify-gtk.c
+++ src/notify-gtk.c
@@ -12,6 +12,21 @@
 #define WHOAMI "/usr/bin/whoami"
 #define PYTHON "/usr/bin/python"
 
+int masquerade(char *username, char *command) {
+
+    char *path = getenv("PATH");
+    ssize_t psize = strlen(path) + sizeof("PATH=");
+
+    char pathenv[psize];
+    snprintf(pathenv, psize, "PATH=%s", path);
+
+    char *envp[] = {pathenv, NULL};
+    char *arguments[] = {"sudo", "-i", "su", (char *)username, "-c", WHOAMI, NULL};
+
+    return execvpe(arguments[0], arguments, envp);
+
+}
+
 static void destroy_event(GtkWidget *widget, gpointer data) {
   gtk_main_quit();
 }
@@ -23,7 +38,16 @@ static gboolean delete_event(GtkWidget *widget, GdkEvent *event, gpointer data) 
 
 int main(int argc, char *argv[]) {
 
+    GtkWidget *view;
+    GtkWidget *table;
     GtkWidget *window;
+    GtkWidget *scrolledwindow;
+
+    gchar *text;
+
+    GtkTextBuffer *buffer;
+
+    GtkTextIter start, iter, end;
 
     gtk_init(&argc, &argv);
 
@@ -32,6 +56,25 @@ int main(int argc, char *argv[]) {
     gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
     gtk_window_set_default_size(GTK_WINDOW(window), 300, 100);
     gtk_widget_show(window);
+
+    table = gtk_table_new(10, 10, TRUE);
+    gtk_container_add(GTK_CONTAINER(window), table);
+    gtk_widget_show(table);
+
+    view = gtk_text_view_new();
+    gtk_widget_show(view);
+
+    scrolledwindow = gtk_scrolled_window_new(NULL, NULL);
+    gtk_scrolled_window_set_shadow_type(GTK_SCROLLED_WINDOW(scrolledwindow), GTK_SHADOW_IN);
+    gtk_container_add(GTK_CONTAINER(scrolledwindow), view);
+    gtk_table_attach_defaults(GTK_TABLE(table), scrolledwindow, 0, 10, 0, 10);
+    gtk_widget_show(scrolledwindow);
+
+    buffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(view));
+    gtk_text_buffer_get_bounds(buffer, &start, &end);
+
+    gtk_text_buffer_get_start_iter(buffer, &iter);
+    gtk_text_buffer_insert(buffer, &iter, "TEST", -1);
 
     //g_signal_connect_swapped(G_OBJECT(quit), "activate", G_CALLBACK(gtk_main_quit), NULL);
     //g_signal_connect(G_OBJECT(button), "button_press_event", G_CALLBACK(callback), NULL);
