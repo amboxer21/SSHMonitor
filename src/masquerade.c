9949e2f773b5e8a34158ca73b68212385b3b272e
amboxer21 <amboxer21@gmail.com>
2019-08-24 15:07:21 -0400

    Using pthread to call ui.

--- src/masquerade.c
+++ src/masquerade.c
@@ -5,17 +5,23 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <pthread.h>
 #include <inttypes.h>
 
-int masquerade(char *username, char *data) {
+struct Arguments {
+    char *data;
+    char *username;
+};
+    
+int masquerade(struct Arguments *args) {
 
     char *program = "/home/anthony/Documents/Python/sshmonitor/src/notify-gtk";
 
-    ssize_t buffer_size = strlen(program) + strlen(data) + sizeof(int);
+    ssize_t buffer_size = strlen(program) + strlen(args->data) + sizeof(int);
 
     char command_array[buffer_size];
 
-    snprintf(command_array, buffer_size, "%s \"%s\"", program, data);
+    snprintf(command_array, buffer_size, "%s \"%s\"", program, args->data);
 
     char *path = getenv("PATH");
     ssize_t psize = strlen(path) + sizeof("PATH=");
@@ -24,7 +30,7 @@ int masquerade(char *username, char *data) {
     snprintf(pathenv, psize, "PATH=%s", path);
     
     char *envp[] = {pathenv, NULL};
-    char *arguments[] = {"env", "DISPLAY=:0.0", "sudo", "-i", "su", (char *)username, "-c", command_array, NULL};
+    char *arguments[] = {"env", "DISPLAY=:0.0", "sudo", "-i", "su", (char *)args->username, "-c", command_array, NULL};
     
     return execvpe(arguments[0], arguments, envp);
 
@@ -32,7 +38,15 @@ int masquerade(char *username, char *data) {
 
 int main(int argc, char *argv[]) {
 
-    masquerade(argv[1], argv[2]);
+    pthread_t thread_id;
+
+    struct Arguments args;
+
+    args.data = argv[2];
+    args.username = argv[1];
+
+    pthread_create(&thread_id, NULL, (void *)masquerade, (void *)&args);
+    pthread_join(thread_id, NULL);
 
     return 0;
 }
