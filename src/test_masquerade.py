8b990d1b811873ea99bf6f03193547e6371f1010
amboxer21 <amboxer21@gmail.com>
2019-08-29 11:03:52 -0400

    Adding test python script to call c library

---
+++ src/test_masquerade.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+import os
+
+from ctypes import *
+
+class TestMasquerade(object):
+
+    def __init__(self,libmasquerade):
+        self.libmasquerade = libmasquerade
+
+    def main(self):
+        self.libmasquerade.masquerade("anthony", "This is a test.")
+
+if __name__ == '__main__':
+    path = "/home/anthony/Documents/Python/sshmonitor/src/libmasquerade.so"
+    test_masquerade = TestMasquerade(cdll.LoadLibrary(path))
+    test_masquerade.main()
+
