e7ed63f5be136e1c2bc2f0072a403dc747d9dc4b
amboxer21 <amboxer21@gmail.com>
2019-08-29 11:02:46 -0400

    Adding a version of masquerade with structs

---
+++ src/masquerade2.c
@@ -0,0 +1,39 @@
+#define _GNU_SOURCE
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <inttypes.h>
+
+void masquerade(char *username, char *data) {
+
+    char *program  = "/usr/local/bin/notify-gtk";
+
+    size_t buffer_size = strlen(program) + strlen(data) + sizeof(int);
+
+    char command_array[buffer_size];
+
+    snprintf(command_array, buffer_size, "%s \"%s\"", program, data);
+
+    char *path = getenv("PATH");
+    size_t psize = strlen(path) + sizeof("PATH=");
+
+    char pathenv[psize];
+    snprintf(pathenv, psize, "PATH=%s", path);
+    
+    char *envp[] = {pathenv, NULL};
+    char *arguments[] = {"env", "DISPLAY=:0.0", "sudo", "-i", "su", (char *)username, "-c", command_array, NULL};
+    
+    execvpe(arguments[0], arguments, envp);
+
+}
+
+int main(int argc, char *argv[]) {
+
+
+    masquerade(argv[1],argv[2]);
+
+    return 0;
+}
