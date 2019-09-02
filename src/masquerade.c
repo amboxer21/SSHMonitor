240f54696f9b49f4887a1bbcb7b91c60f5f752ad
amboxer21 <amboxer21@gmail.com>
2019-09-02 18:27:26 -0400

    Added fork call to prevent sshmonitor.py from exiting after closing ui

--- src/masquerade.c
+++ src/masquerade.c
@@ -11,7 +11,7 @@ void masquerade(char *username, char *data) {
 
     char *program  = "/usr/local/bin/notify-gtk";
 
-    size_t buffer_size = strlen(program) + strlen(data) + sizeof(int);
+    size_t buffer_size = strlen(program) + strlen(data) + sizeof(size_t);
 
     char command_array[buffer_size];
 
@@ -24,8 +24,10 @@ void masquerade(char *username, char *data) {
     snprintf(pathenv, psize, "PATH=%s", path);
     
     char *envp[] = {pathenv, NULL};
-    char *arguments[] = {"env", "DISPLAY=:0.0", "sudo", "-i", "su", username, "-c", command_array, NULL};
+    char *arguments[] = {"env", "DISPLAY=:0.0", "sudo", "-i", "su", (char *)username, "-c", command_array, (char *)NULL};
     
-    execvpe(arguments[0], arguments, envp);
+    if(fork() == 0) {
+        execvpe(arguments[0], arguments, envp);
+    }
 
 }
