1bd9d1557a81b7a3aa1a1f8203d649d54925c07e
amboxer21 <amboxer21@gmail.com>
2019-09-02 23:13:51 -0400

    Added function for getting path to help follow DRY.

--- src/masquerade.c
+++ src/masquerade.c
@@ -7,27 +7,39 @@
 #include <string.h>
 #include <inttypes.h>
 
+char *setpath() {
+
+    char *path = getenv("PATH");
+
+    size_t psize  = strlen(path) + strlen("PATH=") + sizeof(int);
+    char *pathenv = (char *)malloc(sizeof(psize));
+
+    char *ppath = pathenv;
+
+    snprintf(pathenv, psize, "PATH=%s", path);
+
+    free(pathenv);
+
+    return ppath;
+
+}
+
 void masquerade(char *username, char *data) {
 
     char *program  = "/usr/local/bin/notify-gtk";
 
-    size_t buffer_size = strlen(program) + strlen(data) + sizeof(size_t);
+    size_t buffer_size = strlen(program) + strlen(data) + sizeof(int);
+    char *command = (char *)malloc(sizeof(buffer_size));
 
-    char command_array[buffer_size];
+    snprintf(command, buffer_size, "%s \"%s\"", program, data);
 
-    snprintf(command_array, buffer_size, "%s \"%s\"", program, data);
-
-    char *path = getenv("PATH");
-    size_t psize = strlen(path) + sizeof("PATH=");
-
-    char pathenv[psize];
-    snprintf(pathenv, psize, "PATH=%s", path);
-    
-    char *envp[] = {pathenv, NULL};
-    char *arguments[] = {"env", "DISPLAY=:0.0", "sudo", "-i", "su", (char *)username, "-c", command_array, (char *)NULL};
+    char *envp[] = {setpath(), NULL};
+    char *arguments[] = {"env", "DISPLAY=:0.0", "sudo", "-i", "su", username, "-c", command, (char *)NULL};
     
     if(fork() == 0) {
         execvpe(arguments[0], arguments, envp);
     }
+
+    free(command);
 
 }
