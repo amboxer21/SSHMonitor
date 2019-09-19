a8cae29d5b4266cdc138552dd5c8e62a7501276a
amboxer21 <amboxer21@gmail.com>
2019-09-19 14:20:06 -0400

    Updated test_masquerade.py script in src dir.

--- src/test_masquerade.py
+++ src/test_masquerade.py
@@ -1,9 +1,20 @@
 #!/usr/bin/python
 
-import ctypes
+import os
+import time
+
 from ctypes import cdll
 
-path          = "/usr/lib/libmasquerade.so"
-libmasquerade = cdll.LoadLibrary(path)
+class TestMasquerade(object):
 
-libmasquerade.masquerade('anthony','This is a test')
+    def __init__(self,libmasquerade):
+        self.libmasquerade = libmasquerade
+
+    def main(self):
+        self.libmasquerade.masquerade("anthony", "This is a test.")
+
+if __name__ == '__main__':
+    path = "/home/anthony/Documents/Python/sshmonitor/src/testing/libmasquerade2.so"
+    test_masquerade = TestMasquerade(cdll.LoadLibrary(path))
+    test_masquerade.main()
+
