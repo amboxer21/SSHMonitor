c5967491295700743a9f30a80759dd572c722ac4
amboxer21 <amboxer21@gmail.com>
2020-07-27 13:29:48 -0400

    made some more mods to test_build.c

--- src/tests/build.h
+++ src/tests/build.h
@@ -6,8 +6,28 @@ typedef struct Arguments {
     int fd[2];
     int s_stdout;
     char *pkgconfig;
-    char *output[BUFFER];
+    char output[BUFFER];
 } Argument;
+
+char *gsub(char *string, char delim, char n_char) {
+
+    int i;
+    char buffer[BUFFER];
+
+    snprintf(buffer, strlen(string)+1, "%s", string);
+    
+    for(i = 0; i <= strlen(buffer); i++) {
+  		  if(buffer[i] == delim) {
+  			    buffer[i] = (int)n_char;
+ 		    }
+	  }
+
+    char *str = malloc(strlen(buffer)+1);
+    strncpy(str,buffer,strlen(buffer));
+    
+    return str;
+
+}
 
 int length(char **array) {
      int len = *(&array + 1) - array;
