7b08e0dcc5ceae4a69185a35a512afd0e2782511
amboxer21 <amboxer21@gmail.com>
2020-07-28 18:16:20 -0400

    Finished pkgconfig argument formater

--- src/tests/test.c
+++ src/tests/test.c
@@ -5,20 +5,25 @@
 #include <stdlib.h>
 #include <sys/types.h>
 
-int main(int argc, char **argv) {
+#define SIZE 4
+#define BUFFER 4096
 
-    char test[4096] = "-pthread -I/usr/include/gtk-2.0 -I/usr/lib/x86_64-linux-gnu/gtk-2.0/include -I/usr/include/gio-unix-2.0 -I/usr/include/cairo -I/usr/include/pango-1.0 -I/usr/include/atk-1.0 -I/usr/include/cairo -I/usr/include/pixman-1 -I/usr/include/gdk-pixbuf-2.0 -I/usr/include/libmount -I/usr/include/blkid -I/usr/include/pango-1.0 -I/usr/include/harfbuzz -I/usr/include/pango-1.0 -I/usr/include/fribidi -I/usr/include/glib-2.0 -I/usr/lib/x86_64-linux-gnu/glib-2.0/include -I/usr/include/uuid -I/usr/include/freetype2 -I/usr/include/libpng16 -lgtk-x11-2.0 -lgdk-x11-2.0 -lpangocairo-1.0 -latk-1.0 -lcairo -lgdk_pixbuf-2.0 -lgio-2.0 -lpangoft2-1.0 -lpango-1.0 -lgobject-2.0 -lglib-2.0 -lfontconfig -lfreetype";
+char *test_function(char *string) {
 
     int i, j;
     int n = 0;
 
-    char delim[5];
-    char nbuffer[4096]; 
+    char *fbuffer;
+    char delim[SIZE] = "\",\"";
 
-    strncpy(delim,"\",\"",5);
+    char nbuffer[BUFFER]; 
+    char sbuffer[BUFFER]; 
 
-    for(i = 0; i < strlen((char *)test); i++) {
-        if(isspace(test[i])) {
+    strncpy(sbuffer, "\"", 2);
+    strncat(sbuffer, string, strlen(string));
+
+    for(i = 0; i < strlen((char *)sbuffer); i++) {
+        if(isspace(sbuffer[i])) {
             if(n == 0) {
                 n = i;
             }
@@ -27,13 +32,22 @@ int main(int argc, char **argv) {
             nbuffer[n++] = delim[2];
         }
         else {
-            nbuffer[n++] = test[i];
+            nbuffer[n++] = sbuffer[i];
         }
     }
 
-    strcat(nbuffer, "\"");
+    strncat(nbuffer, "\"", 1);
+    fbuffer = strndup(nbuffer, strlen(nbuffer));
 
-    printf("%s\n",nbuffer);
+    return fbuffer;
+
+}
+
+int main(int argc, char **argv) {
+
+    char test[4096] = "-pthread -I/usr/include/gtk-2.0 -I/usr/lib/x86_64-linux-gnu/gtk-2.0/include -I/usr/include/gio-unix-2.0 -I/usr/include/cairo -I/usr/include/pango-1.0 -I/usr/include/atk-1.0 -I/usr/include/cairo -I/usr/include/pixman-1 -I/usr/include/gdk-pixbuf-2.0 -I/usr/include/libmount -I/usr/include/blkid -I/usr/include/pango-1.0 -I/usr/include/harfbuzz -I/usr/include/pango-1.0 -I/usr/include/fribidi -I/usr/include/glib-2.0 -I/usr/lib/x86_64-linux-gnu/glib-2.0/include -I/usr/include/uuid -I/usr/include/freetype2 -I/usr/include/libpng16 -lgtk-x11-2.0 -lgdk-x11-2.0 -lpangocairo-1.0 -latk-1.0 -lcairo -lgdk_pixbuf-2.0 -lgio-2.0 -lpangoft2-1.0 -lpango-1.0 -lgobject-2.0 -lglib-2.0 -lfontconfig -lfreetype";
+
+    test_function(test);
 
     return 0;
 
